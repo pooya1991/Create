7ebc5790257afdbe07329f9927ce91f7b0048fb2
Ali Farid <pooya1991@gmail.com>
2019-09-24 09:32:24 +0330

    get totall followers of an ID

--- R/get_followers.R
+++ R/get_followers.R
@@ -1,19 +1,27 @@
 source("R/utils.R")
 
-get_followers <- function(id, after) {
+get_followers <- function(id, after, k) {
   # define the headers of GET request
   headers <- add_headers(`accept` = "*/*", `accept-encoding` = "gzip, deflate, br", `accept-language` = "en-US,en;q=0.9", 
                          `cookie` = 'ig_cb=1; mid=XYZzPQAEAAHRSJsI6bHlLVQIdu5N; fbm_124024574287414=base_domain=.instagram.com; csrftoken=xZpNV45ToXJzh9ARj3wJPdOATvSSnPME; shbid=1590; shbts=1569092427.7641802; ds_user_id=1275316459; sessionid=1275316459%3A44DZg23qp7ePcg%3A28; rur=FRC; urlgen="{\"134.19.177.25\": 49453}:1iCH6m:rV9_ebexHo_pvV-CpNDuSa4P-A0"', 
-                         `referer` = "https://www.instagram.com/creatorden/followers/", `sec-fetch-mode` = "cors", `sec-fetch-site` = "same-origin",
+                         `sec-fetch-mode` = "cors", `sec-fetch-site` = "same-origin",
                          `user-agent` = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36",
                          `x-csrftoken` = "xZpNV45ToXJzh9ARj3wJPdOATvSSnPME", `x-ig-app-id` = "936619743392459", `x-ig-www-claim` = "hmac.AR0Q3uCAju1vbb_lgktr6vwsFHCiogfYeKgl6X_dQrvtkx4y",
                          `x-requested-with` = "XMLHttpRequest")
   
-  base_url <- "https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=%7B%22id%22%3A%22"
-  rest_url <- "%22%2C%22include_reel%22%3Atrue%2C%22fetch_mutual%22%3Afalse%2C%22first%22%3A50%2C%22after%22%3A%22"
-  last_url <- "%3D%3D%22%7D"
+  if(k > 1) {
+    base_url <- "https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=%7B%22id%22%3A%22"
+    rest_url <- "%22%2C%22include_reel%22%3Atrue%2C%22fetch_mutual%22%3Afalse%2C%22first%22%3A50%2C%22after%22%3A%22"
+    last_url <- "%3D%3D%22%7D"
+    
+    url <- paste0(base_url, id, rest_url, after, last_url)
+  } else {
+    base_url <- "https://www.instagram.com/graphql/query/?query_hash=c76146de99bb02f6415203be841dd25a&variables=%7B%22id%22%3A%22"
+    last_url <- "%22%2C%22include_reel%22%3Atrue%2C%22fetch_mutual%22%3Afalse%2C%22first%22%3A100%7D"
+    
+    url <- paste0(base_url, id, last_url)
+  }
   
-  url <- paste0(base_url, id, rest_url, after, last_url)
   
   req_result <- GET(url, headers)
   req_content <- content(req_result)
---
+++ R/get_followers_total.R
@@ -0,0 +1,26 @@
+get_followers_total <- function(id) {
+  k <- 1
+  t <- 0
+  n_followers <- 2
+  end_cursor <- NULL
+  followers <- list()
+  while(k < n_followers) {
+    res <- get_followers(id = id, after = end_cursor, k = k)
+    followers_df_tem <- res$followers_df
+    end_cursor <- res$end_cursor
+    n_followers <- res$follower_count
+    if(is.null(res$followers_df)) {
+      k <- k + 1
+    } else {
+      k <- k + length(res$followers_df)
+    }
+    t <- t + 1
+    followers[[t]] <- cc$followers_df
+    if(t %% 900 == 0) {
+      Sys.sleep(900)
+    }
+  }
+  followers_total <- do.call(rbind, followers)
+  return(followers_total)
+}
+
