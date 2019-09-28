61a55f949e654716afa72c810fccd982eec26395
Ali Farid <pooya1991@gmail.com>
2019-09-28 20:54:54 +0330

    followers getting works fine

--- R/get_followers_total.R
+++ R/get_followers_total.R
@@ -1,28 +1,32 @@
 source("R/get_followers.R")
 
 get_followers_total <- function(id) {
-  k <- 1
+  k <- 0
   t <- 0
   n_followers <- 2
   end_cursor <- NULL
+  cursors <- vector()
   followers <- list()
   while(k < n_followers) {
     res <- get_followers(id = id, after = end_cursor, k = k)
-    followers_df_tem <- res$followers_df
-    end_cursor <- res$end_cursor
-    n_followers <- res$follower_count
     if(is.null(res$followers_df)) {
-      k <- k + 1
+      Sys.sleep(1)
     } else {
       k <- k + nrow(res$followers_df)
-    }
-    t <- t + 1
-    followers[[t]] <- res$followers_df
-    if(t %% 900 == 0) {
-      Sys.sleep(900)
+      followers_df_tem <- res$followers_df
+      end_cursor <- res$end_cursor
+      n_followers <- res$follower_count
+      t <- t + 1
+      cat(k / n_followers)
+      cursors[t] <- ifelse(is.null(end_cursor), 0, end_cursor)
+      followers[[t]] <- res$followers_df
+      if(t %% 200 == 0) {
+        Sys.sleep(10)
+      }
     }
   }
   followers_total <- do.call(rbind, followers)
+  followers_total <- unique(followers_total)
   return(followers_total)
 }
 
